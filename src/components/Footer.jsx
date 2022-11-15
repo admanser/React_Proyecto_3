ef41cc4a974813349a44e025c9139519102830b8
Nico22202 <105761294+Nico22202@users.noreply.github.com>
2022-11-15 13:35:36 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { FaFacebook, FaTwitterSquare, FaLinkedin,FaInstagram} from "react-icons/fa";
+import {Row, Col, Container} from 'react-bootstrap'
+
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+  <Container fluid className="m-0 p-0" >
+    <Row className="d-flex" style={{ backgroundColor: "#FAA916" }}>
+      <Col className="d-flex justify-content-center align-items-center"><img src="https://trello.com/1/cards/6369ac671b9582011c590418/attachments/6372a44304ff6d018d083864/download/Negro_Blanco_Iconos_Moderno_Festival_de_Cine_Evento_Logotipo_(6).png" width={150}/></Col>
+      <Col>
+        <Row className=" g-3 mt-3">
+          <a className="" href="">link1</a>
+          <a className="" href="">link2</a>
+          <a className="" href="">link3</a>
+        </Row>
+      </Col>
+      <Col>
+        <Row className="  g-3 mt-3 ">
+          <a href="">link4</a>
+          <a href="">link5</a>
+          <a href="">link6</a>
+        </Row>  
+      </Col>
+      <Col>
+        <div className="row mt-5"> 
+          <h3 className="col"> <FaFacebook /></h3>
+          <h3 className="col"> <FaInstagram /></h3>
+          <h3 className="col"> <FaLinkedin /></h3>
+          <h3 className="col"> <FaTwitterSquare/></h3>
+        </div>
+      </Col>
+    </Row>
+    <Row>
+      <h5 className="text-center bg-dark text-light py-2 m-0"> © Copyright 2022 RollingCode Films - All Rights Reserved</h5>
+    </Row>
+  </Container>
+  )
 };
-
+;
 export default Footer;
